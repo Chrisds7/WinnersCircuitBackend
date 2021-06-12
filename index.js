5e87bcb34b0b38f640cafb950100cbee4f95f72e
DeathyVoid <will.iams.sapo@gmail.com>
2021-06-12 12:14:17 -0400

    Updated backend

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require("express");
-const cors = require("cors")
-const app = express()
-const pool = require("./database")
+const { postgraphile } = require("postgraphile");
+const cors = require("cors");
+const app = express();
+const pool = require("./database");
 
+app.use(
+
+	postgraphile(
+
+		"postgres://postgres:admin@localhost:5432/wc_test",
+		"public",
+		{
+			watchPg: true,
+			graphiql: true,
+			enchanceGraphiql: true
+		}
+	)
+
+);
 app.use(cors());
 app.use(express.json());
 
+// GET Signup to check if user exists in database
+app.get("/signup/:userId", async (req, res) => {
+
+	try {
+
+		const user = req.params.userId;
+
+		const userExists = await pool.query("SELECT EXISTS(SELECT * FROM LOGIN_INFO WHERE USERID = $1)",
+
+			[user]
+
+		);
+
+		return res.json(userExists);
+
+	} catch (err) {
+
+		console.log(err);
+
+	}
+
+});
+
+// POST Signup to enter userId into database
 app.post("/signup", async (req, res) => {
 
 	try {
