5853aa194f1dc1036be921bcb6f0d8b405c55066
DeathyVoid <will.iams.sapo@gmail.com>
2021-07-19 11:04:09 -0400

    Changed table columns and updated backend

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ app.post("/api/v1/register", async (req, res) => {
 
 	try {
 
-		const { email, firstName, lastName, dateOfBirth, heightFt, heightIn, school, position } = req.body;
+		const { authId, authName, authFamilyName, authGivenName, email, emailVerified, firstName, lastName, dateOfBirth, heightFt, heightIn, school, position } = req.body;
 
-		const user = await pool.query("INSERT INTO ACCOUNTINFO (EMAIL, FIRSTNAME, LASTNAME, DATEOFBIRTH, HEIGHTINFEET, HEIGHTININCHES, SCHOOL, PLAYERPOSITION) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+		const user = await pool.query(`
 
-			[email, firstName, lastName, dateOfBirth, heightFt, heightIn, school, position]
+			INSERT INTO ACCOUNTINFO 
+			(AUTHID, AUTHNAME, AUTHFAMILYNAME, AUTHGIVENNAME, EMAIL, EMAILVERIFIED, FIRSTNAME, LASTNAME, DATEOFBIRTH, HEIGHTINFEET, HEIGHTININCHES, SCHOOL, PLAYERPOSITION) 
+			VALUES 
+			($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`,
+
+			[authId, authName, authFamilyName, authGivenName, email, emailVerified, firstName, lastName, dateOfBirth, heightFt, heightIn, school, position]
 
 		);
 
@@ -29,16 +34,16 @@ app.post("/api/v1/register", async (req, res) => {
 
 })
 
-// GET Register: Return a user from the database by their email
-app.get("/api/v1/register/:email", async (req, res) => {
+// GET Register: Return a user from the database by their AuthId
+app.get("/api/v1/register/:authId", async (req, res) => {
 
 	try {
 
-		const email = req.params.email;
+		const authId = req.params.authId;
 
-		const user = await pool.query("SELECT * FROM ACCOUNTINFO WHERE EMAIL = $1",
+		const user = await pool.query("SELECT * FROM ACCOUNTINFO WHERE AUTHID = $1",
 
-			[email]
+			[authId]
 
 		);
 
@@ -52,16 +57,16 @@ app.get("/api/v1/register/:email", async (req, res) => {
 
 });
 
-// DELETE Register: Delete a user from the database by their email
-app.delete("/api/v1/register/:email", async (req, res) => {
+// DELETE Register: Delete a user from the database by their AuthId
+app.delete("/api/v1/register/:authId", async (req, res) => {
 
 	try {
 
-		const email = req.params.email;
+		const authId = req.params.authId;
 
-		const user = await pool.query("DELETE FROM ACCOUNTINFO WHERE EMAIL = $1",
+		const user = await pool.query("DELETE FROM ACCOUNTINFO WHERE AUTHID = $1",
 
-			[email]
+			[authId]
 
 		);
 
@@ -75,17 +80,23 @@ app.delete("/api/v1/register/:email", async (req, res) => {
 
 });
 
-app.put("/api/v1/register/:email", async (req, res) => {
+app.put("/api/v1/register/:authId", async (req, res) => {
 
 	try {
 
-		const email = req.params.email;
+		const authId = req.params.authId;
 
-		const { firstName, lastName, dateOfBirth, heightFt, heightIn, school, position } = req.body;
+		const { authName, authFamilyName, authGivenName, email, emailVerified, firstName, lastName, dateOfBirth, heightFt, heightIn, school, position } = req.body;
 
-		const user = await pool.query("UPDATE ACCOUNTINFO SET FIRSTNAME = $1, LASTNAME = $2, DATEOFBIRTH = $3, HEIGHTINFEET = $4, HEIGHTININCHES = $5, SCHOOL = $6, PLAYERPOSITION = $7 WHERE EMAIL = $8", 
+		const user = await pool.query(`
 
-			[firstName, lastName, dateOfBirth, heightFt, heightIn, school, position, email]
+			UPDATE ACCOUNTINFO SET 
+			AUTHNAME = $1, AUTHFAMILYNAME = $2, AUTHGIVENNAME = $3, EMAIL = $4, EMAILVERFIED = $5, 
+			FIRSTNAME = $6, LASTNAME = $7, DATEOFBIRTH = $8, HEIGHTINFEET = $9, HEIGHTININCHES = $10, 
+			SCHOOL = $11, PLAYERPOSITION = $12 
+			WHERE AUTHID = $13`, 
+
+			[authName, authFamilyName, authGivenName, email, emailVerified, firstName, lastName, dateOfBirth, heightFt, heightIn, school, position, authId]
 
 		);
 
